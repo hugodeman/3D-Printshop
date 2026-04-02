412f52641ab8a9babf6531c4739fabb6ffea48df
hugodeman <1078703@hr.nl>
2026-04-02 12:54:12 +0200

    Database connection established

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,12 @@
-// import { PrismaClient } from "@prisma/client"
-//
-// const globalForPrisma = global as unknown as { prisma: PrismaClient }
-//
-// export const prisma =
-//     globalForPrisma.prisma || new PrismaClient()
-//
-// if (process.env.NODE_ENV !== "production")
-//     globalForPrisma.prisma = prisma
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/app/generated/prisma/client";
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+})
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const prisma = globalForPrisma.prisma || new PrismaClient({
+    adapter,
+})
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+export default prisma
